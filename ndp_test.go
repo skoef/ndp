2f6fc313d6f20a9e4e1b127f93618d3232c2f839
Reinier Schoof <reinier@skoef.nl>
2016-12-21 20:26:41 +0100

    additional encDomainName tests

--- ndp_test.go
+++ ndp_test.go
@@ -31,4 +31,38 @@ func TestEncDecDomainName(t *testing.T) {
 			t.Errorf("failed to decode %v to %s, result was %s", test.encoded, test.name, name)
 		}
 	}
+
+	// total length may not exceed 255 bytes
+	encoded := encDomainName([]string{
+		// many labels
+		"aaaa.aaaa.aaaa",
+		"bbbb.bbbb.bbbb",
+		"cccc.cccc.cccc",
+		"dddd.dddd.dddd",
+		"eeee.eeee.eeee",
+		"ffff.ffff.ffff",
+		"gggg.gggg.gggg",
+		"hhhh.hhhh.hhhh",
+		"iiii.iiii.iiii",
+		"jjjj.jjjj.jjjj",
+		"kkkk.kkkk.kkkk",
+		"llll.llll.llll",
+		"mmmm.mmmm.mmmm",
+		"nnnn.nnnn.nnnn",
+		"oooo.oooo.oooo",
+		"pppp.pppp.pppp",
+		"qqqq.qqqq.qqqq",
+	})
+	if len(encoded) != 255 {
+		t.Errorf("expected truncated encoding of 255, not %d", len(encoded))
+	}
+
+	// individual label length may not exceed 63 bytes
+	encoded = encDomainName([]string{
+		// very long label
+		"abcdefghijlmnopqrstuvwyxzabcdefghijlmnopqrstuvwyxzabcdefghijlmnopqrstuvwyxz.foo",
+	})
+	if len(encoded) != 72 {
+		t.Errorf("expected truncated encoding of 72, not %d", len(encoded))
+	}
 }
